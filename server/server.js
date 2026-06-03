7e54495fd178904a2593109a934fca88941cd30f
Piyush <piyushgautamgr8@gmail.com>
2026-06-03 23:39:52 +0530

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Gallery of Wonders API");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
