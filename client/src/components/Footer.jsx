e3b98067e6d222006f1e321a1665ac5a1204da6b
Piyush <piyushgautamgr8@gmail.com>
2026-06-06 01:32:34 +0530

    style(ui): establish global design system and responsive layout

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer>
+    <footer className="footer">
       <p>Gallery of Wonders footer placeholder.</p>
     </footer>
   )
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -13,49 +13,51 @@ function Navbar() {
   }
 
   return (
-    <nav>
-      <NavLink to="/" className={getNavLinkClassName}>
-        Gallery of Wonders
-      </NavLink>
-      <ul>
-        <li>
-          <NavLink to="/" className={getNavLinkClassName}>
-            Home
-          </NavLink>
-        </li>
-        {isAuthenticated ? (
-          <>
-            <li>
-              <NavLink to="/gallery" className={getNavLinkClassName}>
-                Gallery
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/profile" className={getNavLinkClassName}>
-                Profile
-              </NavLink>
-            </li>
-            <li>
-              <button type="button" onClick={handleLogout}>
-                Logout
-              </button>
-            </li>
-          </>
-        ) : (
-          <>
-            <li>
-              <NavLink to="/login" className={getNavLinkClassName}>
-                Login
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/register" className={getNavLinkClassName}>
-                Register
-              </NavLink>
-            </li>
-          </>
-        )}
-      </ul>
+    <nav className="navbar">
+      <div className="navbar__inner">
+        <NavLink to="/" className="navbar__logo">
+          Gallery of Wonders
+        </NavLink>
+        <ul className="navbar__links">
+          <li>
+            <NavLink to="/" className={getNavLinkClassName}>
+              Home
+            </NavLink>
+          </li>
+          {isAuthenticated ? (
+            <>
+              <li>
+                <NavLink to="/gallery" className={getNavLinkClassName}>
+                  Gallery
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/profile" className={getNavLinkClassName}>
+                  Profile
+                </NavLink>
+              </li>
+              <li>
+                <button type="button" onClick={handleLogout}>
+                  Logout
+                </button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <NavLink to="/login" className={getNavLinkClassName}>
+                  Login
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/register" className={getNavLinkClassName}>
+                  Register
+                </NavLink>
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
     </nav>
   )
 }
